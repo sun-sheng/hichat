7f1a777eb09cc6e516be8d909f8e6c9e58f4d98e
sun-sheng <sunsheng.cn@gmail.com>
2015-08-25 15:06:20 +0800

    update bin

---
+++ bin/test.js
@@ -0,0 +1,30 @@
+var User    = require('../models').user;
+var Chat    = require('../models').chat;
+var Message = require('../models').message;
+var _       = require('lodash');
+var Q       = require('q');
+
+function countUsers () {
+  User.count(function (err, count) {
+    if (err) return console.log('count users error : ' + err);
+    console.log('the users count is : ' + count);
+  });
+}
+
+function findMessages () {
+  Message.find({chat_id:'1ea848c2-4973-11e5-9e8f-41206a0ae18c'}).then(function (results) {
+    console.log(results.length);
+    _.each(results, function (item) {
+      console.log(item.receiver_id === '1fd3a961-4973-11e5-9e8f-41206a0ae18c');
+    })
+  });
+}
+
+function findAdmin () {
+  User.findOne({name: 'admin'}, function (err, user) {
+    if (err) return console.log(err);
+    console.log(user.id);
+  });
+}
+
+findAdmin();
